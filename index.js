c94867296d6057ba305976e5cd0a9fc3aded68dd
Kristina Stout <kristinalstout@gmail.com>
2023-05-18 22:27:31 -0400

    Created cat functions

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+destructivelyRemoveLastCat(-1);
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+destructivelyRemoveFirstCat(0);
+
+function appendCat(name){
+    const allCats = [...cats, (name)];
+    return allCats;
+}
+appendCat("Broom");
+
+function prependCat(name){
+    const newCats = [(name), ...cats];
+    return newCats
+}
+prependCat("Arnold");
+
+function removeLastCat(){
+    const updatedCats = [...cats];
+    updatedCats.pop();
+    return updatedCats;
+}
+removeLastCat(-1);
+
+function removeFirstCat(){
+    const firstCat = [...cats];
+    firstCat.shift();
+    return firstCat;
+}
+removeFirstCat(0);
